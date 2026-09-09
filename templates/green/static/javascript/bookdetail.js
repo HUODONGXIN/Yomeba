ec25c52a8ed654dad260b918a0e1701729b217fd
h <h>
2026-09-09 19:07:47 +0900

    F1~F9: PC门户与书籍页面日语化
    - 首页/404/分类/排行/详情/阅读/目录/评论区/评论回复 9 页面 + bookclass.js/bookdetail.js
    - 站名占位: 小説ハウス, 货币占位: コイン
    - 分类字典: 玄幻=ファンタジー 等 12 项; 通用汉字(都市/仙侠/武侠/同人)保留
    - 全部用户可见文案及 JS 提示语已日语化, 中文残留扫描通过, node --check 通过

--- templates/green/static/javascript/bookdetail.js
+++ templates/green/static/javascript/bookdetail.js
@@ -8,12 +8,12 @@
     DescriptionMore: function (sClass) {
         if (sClass == "") {
             if ($("#pDesMore").html().length > 150) {
-                $("#divDescription").html($("#pDesMore").html().substring(0, 150) + "<a href=\"javascript:void(0);\" class=\"info_txt_more\" onclick=\"javascript:BookDetail.DescriptionMore('down');\">展开<img src=\"../images/arrow_d.png\" /></a>");
+                $("#divDescription").html($("#pDesMore").html().substring(0, 150) + "<a href=\"javascript:void(0);\" class=\"info_txt_more\" onclick=\"javascript:BookDetail.DescriptionMore('down');\">開く<img src=\"../images/arrow_d.png\" /></a>");
             } else {
                 $("#divDescription").html($("#pDesMore").html());
             }
         } else {
-            $("#divDescription").html($("#pDesMore").html() + "<a href=\"javascript:void(0);\"  class=\"info_txt_more\"  onclick=\"javascript:BookDetail.DescriptionMore('');\">收起<img src=\"../images/arrow_t.png\" /></a>");
+            $("#divDescription").html($("#pDesMore").html() + "<a href=\"javascript:void(0);\"  class=\"info_txt_more\"  onclick=\"javascript:BookDetail.DescriptionMore('');\">閉じる<img src=\"../images/arrow_t.png\" /></a>");
         }
     },
     AddFavorites: function (BId, CId, layerStatus) {
@@ -25,15 +25,15 @@
             success: function (data) {
                 if (data.code == 200) {
                     if (layerStatus == 1) {
-                        $("#cFavs").html("<a class=\"ico_shelf\" href=\"javascript:void(0);\"><b>已收藏</b></a>");
+                        $("#cFavs").html("<a class=\"ico_shelf\" href=\"javascript:void(0);\"><b>お気に入り登録済み</b></a>");
                         jQuery.cookie("u-faorites", "1");
                     } else {
-                        $("#cFavs").html("<a class=\"btn_ora_white btn_addsj\" href=\"javascript:void(0);\">已在书架</a>");
+                        $("#cFavs").html("<a class=\"btn_ora_white btn_addsj\" href=\"javascript:void(0);\">本棚に追加済み</a>");
                     }
 
 
                 } else if (data.code == 1001) {
-                    //未登录
+                    //未ログイン
                     location.href = '/user/login.html?originUrl=' + encodeURIComponent(location.href);
 
                 } else {
@@ -42,7 +42,7 @@
 
             },
             error: function () {
-                layer.alert('网络异常');
+                layer.alert('ネットワークエラー');
             }
         })
     },
@@ -50,7 +50,7 @@
         if (jQuery.cookie("u-faorites") == null) {
         } else {
             if (jQuery.cookie("u-faorites") == "1") {
-                $("#cFavs").html("<a class=\"ico_shelf\" href=\"javascript:void(0);\"><b>已加书架</b></a>");
+                $("#cFavs").html("<a class=\"ico_shelf\" href=\"javascript:void(0);\"><b>本棚に追加済み</b></a>");
             }
         }
         /*BookDetail.SetWholeTip();*/
@@ -63,48 +63,48 @@
         if (mTotal != undefined) {
             var iTotal = parseInt(mTotal);
             if (iTotal < 500) {
-                return ["user_level1", "见习"];
+                return ["user_level1", "見習い"];
             } else if (iTotal < 2000) {
-                return ["user_level2", "学徒"];
+                return ["user_level2", "徒弟"];
             } else if (iTotal < 5000) {
                 return ["user_level3", "弟子"];
             } else if (iTotal < 10000) {
-                return ["user_level4", "执事"];
+                return ["user_level4", "執事"];
             } else if (iTotal < 20000) {
                 return ["user_level5", "舵主"];
             } else if (iTotal < 30000) {
                 return ["user_level6", "堂主"];
             } else if (iTotal < 40000) {
-                return ["user_level7", "护法"];
+                return ["user_level7", "護法"];
             } else if (iTotal < 50000) {
-                return ["user_level8", "长老"];
+                return ["user_level8", "長老"];
             } else if (iTotal < 70000) {
-                return ["user_level9", "掌门"];
+                return ["user_level9", "掌門"];
             } else if (iTotal < 100000) {
-                return ["user_level10", "宗师"];
+                return ["user_level10", "宗師"];
             } else {
                 return ["user_level11", "盟主"];
             }
         } else {
-            return ["user_level1", "见习"];
+            return ["user_level1", "見習い"];
         }
     },
     SaveComment: function (cmtBId, cmtCId, cmtDetail) {
         if (!isLogin) {
-            layer.alert('请先登录');
+            layer.alert('ログインしてください');
             return;
         }
         var cmtDetailTemp = cmtDetail.replace(/(^\s*)/g, "");
         if (cmtDetailTemp == '') {
-            layer.alert('评论内容必须填写');
+            layer.alert('レビュー内容を入力してください');
             return;
         }
         if (cmtDetailTemp.length < 5) {
-            layer.alert('评论内容必须大于5个字');
+            layer.alert('レビュー内容は5文字以上で入力してください');
             return;
         }
         if (cmtDetail.length < 5) {
-            layer.alert('评论内容必须大于5个字');
+            layer.alert('レビュー内容は5文字以上で入力してください');
             return;
         }
         $.ajax({
@@ -115,11 +115,11 @@
             success: function (data) {
                 if (data.code == 200) {
                     $('#txtComment').val("")
-                    layer.alert('评价成功！');
+                    layer.alert('レビューを投稿しました！');
                     loadCommentList();
 
                 } else if (data.code == 1001) {
-                    //未登录
+                    //未ログイン
                     location.href = '/user/login.html?originUrl=' + encodeURIComponent(location.href);
 
                 } else {
@@ -128,7 +128,7 @@
 
             },
             error: function () {
-                layer.alert('网络异常');
+                layer.alert('ネットワークエラー');
             }
         })
 
@@ -137,20 +137,20 @@
 
     SaveCommentReply: function (cmtBId, cmtCId, cmtDetail) {
         if (!isLogin) {
-            layer.alert('请先登录');
+            layer.alert('ログインしてください');
             return;
         }
         var cmtDetailTemp = cmtDetail.replace(/(^\s*)/g, "");
         if (cmtDetailTemp == '') {
-            layer.alert('回复内容必须填写');
+            layer.alert('返信内容を入力してください');
             return;
         }
         if (cmtDetailTemp.length < 5) {
-            layer.alert('回复内容必须大于5个字');
+            layer.alert('返信内容は5文字以上で入力してください');
             return;
         }
         if (cmtDetail.length < 5) {
-            layer.alert('回复内容必须大于5个字');
+            layer.alert('返信内容は5文字以上で入力してください');
             return;
         }
         $.ajax({
@@ -161,11 +161,11 @@
             success: function (data) {
                 if (data.code == 200) {
                     $('#txtComment').val("")
-                    layer.alert('回复成功！');
+                    layer.alert('返信しました！');
                     loadCommentList(1, 20);
 
                 } else if (data.code == 1001) {
-                    //未登录
+                    //未ログイン
                     location.href = '/user/login.html?originUrl=' + encodeURIComponent(location.href);
 
                 } else {
@@ -174,7 +174,7 @@
 
             },
             error: function () {
-                layer.alert('网络异常');
+                layer.alert('ネットワークエラー');
             }
         })
 
@@ -332,16 +332,16 @@
         }
     },
     SetWholeTip: function () {
-        var str = '<li>1、此书为全网优质作品，按照整本定价折扣销售，购买之后可以阅读该书全部章节。</li>';
-        str += '<li>2、支付屋币即可阅读收费章节，没有屋币的需要先充值。</li>';
-        str += '<li>3、QQ、微信、微博3种账号之间的数据不互通，如果你发现充值成功但没有屋币到账，请切换账号查看是否充到了别的账号中。</li>';
+        var str = '<li>1、この作品はサイト内の高品質作品です。一括定価の割引価格で販売しており、購入後は全話を読むことができます。</li>';
+        str += '<li>2、コインを支払うと有料話を読めます。コインがない場合は先にチャージしてください。</li>';
+        str += '<li>3、QQ・WeChat・Weiboの3つのアカウント間でデータは連携されません。チャージが成功したのにコインが反映されない場合は、他のアカウントにチャージされていないか確認してください。</li>';
         if ($("#HidIsWholeBook").val() == "1") {
             $(".tip_list").html(str);
         }
     },
     SetDZChapter: function (bId, cId, isDianZan) {
         if (isDianZan == 1) {
-            /*是点赞，设置点赞数+1和不可点状态*/
+            /*いいねの場合、いいね数+1とクリック不可状態に設定*/
             var dzData = parseInt($("#read_dz_bar a").text()) + 1;
             $("#read_dz_bar").html('<a class="read_dz on" href="javascript:void(0)"><i></i>' + dzData + '</a>');
         }
