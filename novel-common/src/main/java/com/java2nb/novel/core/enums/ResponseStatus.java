628735a35efe38538c4ea29c1eaa9be9618e41cf
h <h>
2026-09-21 20:41:16 +0900

    后端API错误消息全部日语化（ResponseStatus枚举）

--- novel-common/src/main/java/com/java2nb/novel/core/enums/ResponseStatus.java
+++ novel-common/src/main/java/com/java2nb/novel/core/enums/ResponseStatus.java
@@ -18,48 +18,48 @@ public enum ResponseStatus implements IResultCode {
     /**
      * 用户相关错误
      * */
-   NO_LOGIN(1001, "未登录或登录失效！"),
-    VEL_CODE_ERROR(1002, "验证码错误！"),
-    USERNAME_EXIST(1003,"该手机号已注册！"),
-    USERNAME_PASS_ERROR(1004,"手机号或密码错误！"),
-    TWO_PASSWORD_DIFF(1005, "两次输入的新密码不匹配!"),
-    OLD_PASSWORD_ERROR(1006, "旧密码不匹配!"),
-    USER_NO_BALANCE(1007, "用户余额不足"),
+   NO_LOGIN(1001, "ログインしていません"),
+    VEL_CODE_ERROR(1002, "認証コードが不正です"),
+    USERNAME_EXIST(1003,"この携帯番号は既に登録されています"),
+    USERNAME_PASS_ERROR(1004,"携帯番号またはパスワードが違います"),
+    TWO_PASSWORD_DIFF(1005, "新しいパスワードが一致しません"),
+    OLD_PASSWORD_ERROR(1006, "現在のパスワードが違います"),
+    USER_NO_BALANCE(1007, "残高が不足しています"),
 
     /**
      * 评论相关错误
      * */
-    HAS_COMMENTS(3001, "已评价过该书籍！"),
+    HAS_COMMENTS(3001, "この本は既に評価済みです"),
 
     /**
      * 作者相关错误
      * */
-    INVITE_CODE_INVALID(4001, "邀请码无效！"),
-    AUTHOR_STATUS_FORBIDDEN(4002, "作者状态异常，暂不能管理小说！")
-    , BOOKNAME_EXISTS(4003,"已发布过同名小说！"),
+    INVITE_CODE_INVALID(4001, "招待コードが無効です"),
+    AUTHOR_STATUS_FORBIDDEN(4002, "作者アカウントの状態が正常でないため、作品を管理できません")
+    , BOOKNAME_EXISTS(4003,"同名の作品は既に公開されています"),
 
     /**
      * 小说相关错误
      */
-    BOOK_EXISTS(5001,"该小说已存在")
+    BOOK_EXISTS(5001,"この作品は既に存在します")
 
             ,
     /**
      * 搜索引擎相关错误
      * */
-    ES_SEARCH_FAIL(9001,"搜索引擎查询错误！"),
+    ES_SEARCH_FAIL(9001,"検索エンジンでエラーが発生しました"),
 
     /**
      * 文件相关错误
      * */
-    FILE_DIR_MAKE_FAIL(10001,"目录创建失败"),
-    FILE_NOT_IMAGE(10002,"请上传图片类型的文件"),
-    FILE_SIZE_LIMIT(10003,"文件大小超出限制"),
+    FILE_DIR_MAKE_FAIL(10001,"ディレクトリの作成に失敗しました"),
+    FILE_NOT_IMAGE(10002,"画像ファイルをアップロードしてください"),
+    FILE_SIZE_LIMIT(10003,"ファイルサイズが制限を超えています"),
 
     /**
      * 其他通用错误
      * */
-    PASSWORD_ERROR(88001,"密码错误！");
+    PASSWORD_ERROR(88001,"パスワードが違います");
 
     private int code;
     private String msg;
